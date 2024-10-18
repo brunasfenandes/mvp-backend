29a215a78065c51f25eafe84f374dab19eba0ab6
Okashe <okashabdi88@gmail.com>
2024-10-17 22:28:28 -0500

    add routes & controllers(empty)

--- index.js
+++ index.js
@@ -2,24 +2,22 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import roomRoutes from "./routes/room-routes.js";
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-
-
 app.use(cors());
 
 //parse data
 app.use(express.json());
 
-
+//chatrooms route
+app.use("/room", roomRoutes)
 // Basic API route
 app.get('/', (_req, _res) => {
   _res.send('Welcome to the MVP API');
 });
-
 
 // Start the server
 const PORT = process.env.PORT || 8080;
